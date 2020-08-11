549dc602704293b4b60852491b2ce179bb9ba374
daehoanshin <xbb123@naver.com>
2020-08-11 11:06:09 +0900

    스프링 부트 2.0 Day 14 HttpMessageConverters 부가 설명 그리고 Static 리소스 맵핑과 위치 변경 진행중

--- src/main/java/me/xbb123/config/WebConfig.java
+++ src/main/java/me/xbb123/config/WebConfig.java
@@ -3,9 +3,7 @@ package me.xbb123.config;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -15,9 +13,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
-    public HttpMessageConverters customConverters() {
-        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
-        return new HttpMessageConverters(converter);
-    }
 }
--- src/main/java/me/xbb123/user/UserController.java
+++ src/main/java/me/xbb123/user/UserController.java
@@ -2,7 +2,9 @@ package me.xbb123.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,8 +24,17 @@ public class UserController {
      * @return
     * */
     @RequestMapping("/")
-    public String index() {
+    public @ResponseBody String index() {
         converters.getConverters().forEach(c -> System.out.println(";;;;;;;; " + c.getClass()));
         return "Hello Spring MVC";
+    }
+
+    @GetMapping("/user")
+    public User currentUser() {
+        User user = new User();
+        user.setAge(20);
+        user.setName("keesun");
+        user.setHeight(175);
+        return user;
     }
 }
