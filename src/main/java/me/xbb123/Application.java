e000610a1f49344ca9ee75189be7a86006542de2
daehoanshin <xbb123@naver.com>
2020-08-19 22:00:38 +0900

    공백 테스트

--- src/main/java/me/xbb123/Application.java
+++ src/main/java/me/xbb123/Application.java
@@ -24,6 +24,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
         application.run(args);
+
     }
 
     @Bean
