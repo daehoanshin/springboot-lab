a6468a0fcda618ce45a63a1d4a260ae6aca35c64
daehoanshin <xbb123@naver.com>
2020-08-20 12:50:04 +0900

    template add

---
+++ template/src/main/java/me/xbb123/Application.java
@@ -0,0 +1,14 @@
+package me.xbb123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xbb123
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
