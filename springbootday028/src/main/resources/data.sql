34717afc240bebc3591029fd5f4f409a3a6c3da0
daehoanshin <xbb123@naver.com>
2020-08-22 11:12:35 +0900

    스프링 부트 2.0 Day 28. 스프링 JDBC와 인메모리 DB 완료

---
+++ springbootday028/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email) VALUES ('mkyong', 'aaa@gmail.com');
+INSERT INTO users (name, email) VALUES ('alex', 'bbb@gmail.com');
+INSERT INTO users (name, email) VALUES ('joel', 'ccc@gmail.com');
