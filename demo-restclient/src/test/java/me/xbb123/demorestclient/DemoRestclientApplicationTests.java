d66bc5bc7120978abe3ed6ec9a5bb7117fc017c7
daehoanshin <xbb123@naver.com>
2020-08-26 19:33:07 +0900

    스프링 부트 2.0 Day 34. RestTemplate과 WebClient 완료

---
+++ demo-restclient/src/test/java/me/xbb123/demorestclient/DemoRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xbb123.demorestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
