c2bbe8303b3af70f71c6109017c6a28fb17b1b74
daehoanshin <xbb123@naver.com>
2020-08-26 22:43:22 +0900

    스프링 부트 2.0 Day 35. 스프링 부트 Actuator 1부 완료

---
+++ demoactuator/src/main/java/me/xbb123/demoactuator/DemoactuatorApplication.java
@@ -0,0 +1,13 @@
+package me.xbb123.demoactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoactuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoactuatorApplication.class, args);
+    }
+
+}
---
+++ demoactuator/src/test/java/me/xbb123/demoactuator/DemoactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.xbb123.demoactuator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoactuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
